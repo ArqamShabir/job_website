8b55e90d5c03e10923a2a3ccef1ab37c3efbf9d4
hadiyaAzmat23 <162620972+hadiyaAzmat23@users.noreply.github.com>
2024-07-12 12:24:31 +0500

    Update database.sql

--- Hadia/v1/database/database.sql
+++ Hadia/v1/database/database.sql
@@ -1,1 +1,23 @@
 
+CREATE DATABASE job_seeking;
+USE job_seeking;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    gender ENUM('Male', 'Female'),
+    email VARCHAR(100) UNIQUE,
+    country_code VARCHAR(10),
+    phone_number VARCHAR(15),
+    password VARCHAR(100)
+);
+CREATE TABLE companies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone_number VARCHAR(255) NOT NULL,
+    country_code VARCHAR(10) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    registration_number VARCHAR(255) NOT NULL,
+    logo VARCHAR(255) NOT NULL
+);
