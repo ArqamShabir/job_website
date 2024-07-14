b67f043b0ef77df308aa032f4dceeee8a8d040b6
Eman-333 <221400020@gift.edu.pk>
2024-07-15 00:22:23 +0500

    Update CompanyLogin.js

--- Eman/components/CompanyLogin.js
+++ Eman/components/CompanyLogin.js
@@ -1,1 +1,35 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
+const CompanyLogin = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post('http://localhost:3001/company-login', formData)
+            .then(response => {
+                alert(response.data);
+            })
+            .catch(error => {
+                console.error("There was an error logging in the company!", error);
+            });
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h2>Company Login</h2>
+            <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
+            <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default CompanyLogin;
