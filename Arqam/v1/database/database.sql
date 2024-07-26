d12b35c43572dc1fdb95a26b2f7cdf53dc278ff3
Mohammad Arqam <122014225+ArqamShabir@users.noreply.github.com>
2024-07-26 09:47:13 +0500

    Update database.sql

--- Arqam/v1/database/database.sql
+++ Arqam/v1/database/database.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE job_seeking;
 USE job_seeking;
-
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50),
@@ -11,12 +10,26 @@ CREATE TABLE users (
     phone_number VARCHAR(15),
     password VARCHAR(100)
 );
+
 CREATE TABLE companies (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    phone_number VARCHAR(255) NOT NULL,
-    country_code VARCHAR(10) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    registration_number VARCHAR(255) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    registrationNumber int(15) PRIMARY KEY,
+    phoneNumber VARCHAR(20) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    register_date date DEFAULT CURRENT_DATE, 
+    password VARCHAR(50) NOT NULL,
+    c_status BOOLEAN NOT NULL DEFAULT FALSE,
     logo VARCHAR(255) NOT NULL
 );
+
+INSERT INTO companies(companies.name,companies.registrationNumber,companies.phoneNumber,companies.email,companies.password)
+VALUES ("Coke",123,"123","123","123") , ("Apple",124,"124","124","124");
+
+CREATE TABLE admin(
+	username varchar(25) PRIMARY KEY,
+    password varchar(25) NOT NULL,
+    noOfRows int(1) DEFAULT 1 UNIQUE
+);
+
+INSERT INTO admin(admin.username,admin.password) 
+VALUES("admin","admin");
