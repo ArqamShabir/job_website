0e7f7a7f7e53a09b5290ad3656c89395a368dd77
Shareen Faisal <128238636+sha-reen@users.noreply.github.com>
2024-08-01 21:36:47 +0500

    Update AdminLogin.js

--- Shareen/components/AdminLogin.js
+++ Shareen/components/AdminLogin.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './adminLoginStyle.css';
+
 
 const AdminLogin = () => {
 
@@ -7,32 +9,56 @@ const AdminLogin = () => {
             username:"",
             password:""
         });
+        const [errorMessage, setErrorMessage] = useState('');
+
 
         const handleChange = e => {
             setFormData({...formData,[e.target.name] : e.target.value});
+            setErrorMessage(''); 
         };
 
         const handleSubmit = e => {
             e.preventDefault();
+            setErrorMessage('');
             axios.post('http://localhost:3001/admin-login',formData)
 
                 .then(response =>{
-                    //alert(response.data);
                     window.location.href = '/AdminDashboard';
                 })
 
                 .catch(error => {
+                    setErrorMessage('Incorrect username or password');
                     console.error('There was an error loging in for admin!',error);
                 })
         };
 
         return (
-            <form onSubmit={handleSubmit} className="form-container">
-                <h2>Admin Login</h2>
-                <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
-                <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
-                <button type="submit">Login</button>
-            </form>
+
+            <div className="c_login_main">
+            <div className="left">
+                <header className="header" style={{ justifyContent: 'center', position: 'absolute', top: 0, width: '100%', boxShadow: 'none' }}>
+                    <div className="logo">
+                        <img src="logo.png" alt="Logo" />
+                        <h3 style={{ marginTop: '2px' }}>LabourLink</h3>
+                    </div>
+                </header>
+                <div>
+                <h2 id="admin-login-h2">Login</h2>
+
+                </div>
+
+
+                <form onSubmit={handleSubmit} className="form-container">
+                    <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
+                    <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+                    {errorMessage && <div className="error-messages" style={{ color: 'red' }}>{errorMessage}</div>}
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+            <div className="right">
+                <img src="login_image.png" alt="Image" />
+            </div>
+        </div>
         );
     
 
@@ -40,4 +66,3 @@ const AdminLogin = () => {
 }//adminlogin
 
 export default AdminLogin;
-
